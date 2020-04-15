6e98ef1e74812582a649a1ddcedb6cf7fa1346ab
James Dobbe <dobbe2@gmail.com>
2020-04-15 14:40:07 -0700

    cleaned up logs and space

--- models/workout.js
+++ models/workout.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
+
 const workoutSchema = new Schema(
   {
     day: {
@@ -38,12 +40,10 @@ const workoutSchema = new Schema(
     ]
   },
   {
-    toJSON: {
-      // include any virtual properties when data is requested
-      virtuals: true
-    }
+    toJSON: { virtuals: true }
   }
 );
+//slacked from tutor for this section
 // adds a dynamically-created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
   // "reduce" array of exercises down to just the sum of their durations
@@ -54,53 +54,3 @@ workoutSchema.virtual("totalDuration").get(function () {
 const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
 
-// const mongoose = require("mongoose");
-
-// const Schema = mongoose.Schema;
-
-// const workoutSchema = new Schema({
-//     date: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     exercise: [
-//         {
-//             type: {
-//                 type: String,
-//                 trim: true,
-//                 required: "Cardio or Resistance?"
-//             },
-//             name: {
-//                 type: String,
-//                 trim: true,
-//                 required: "Enter the name of your exercise"
-//             },
-//             duration: {
-//                 type: Number,
-//                 required: "How long is your exercise?"
-//             },
-//             distance: {
-//                 type: Number
-//             },
-//             weight: {
-//                 type: Number
-//             },
-//             reps: {
-//                 type: Number
-//             },
-//             sets: {
-//                 type: Number
-//             }
-//         }
-//     ]
-// });
-
-// workoutSchema.set('toJSON', { virtuals: true });
-
-// workoutSchema.virtual('totalDuration')
-//     .get(function(){return this.exercises.reduce((total, exercise) => 
-//         { return total + exercise.duration }, 0)})
-
-// const Workout = mongoose.model("workout", workoutSchema);
-
-// module.exports = Workout;
--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,10 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req,res) => {
-  // res.json({"hello": "hi there"})
     Workout.find()
     .then(dbWorkout => {
-        console.log("HIT HERE");
-        console.log("get all workouts" , dbWorkout);
+        // console.log("HIT HERE");
+        // console.log("get all workouts" , dbWorkout);
         res.json(dbWorkout);
     })
     .catch(err => {
@@ -15,10 +14,10 @@ router.get("/api/workouts", (req,res) => {
 });
 
 router.post("/api/workouts", ( {body} , res) => {
-    console.log("post new workout", {body});
+    // console.log("post new workout", {body});
     Workout.create({})
       .then(workout => {
-        console.log(workout)
+        // console.log(workout)
         res.json(workout);
       })
       .catch(err => {
@@ -29,7 +28,7 @@ router.post("/api/workouts", ( {body} , res) => {
   router.put("/api/workouts/:id", ({ body, params }, res) => {
     Workout.findOneAndUpdate({ _id: params.id }, {$push: {exercises: body}}, {new:true})
     .then(workout => {
-      console.log("update workout", workout);
+      // console.log("update workout", workout);
       res.json(workout)
     }).catch(err => {
       res.status(400).json(err);
@@ -39,7 +38,7 @@ router.post("/api/workouts", ( {body} , res) => {
   router.get("/api/workouts/range", (req, res) => {
     Workout.find({})
     .then(dbWorkout => {
-      console.log("get range", dbWorkout);
+      // console.log("get range", dbWorkout);
       res.json(dbWorkout);
     })
     .catch(err => {
